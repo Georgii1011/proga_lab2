a238085e66c818634abef2fae07dee911559f758
Георгий <georgij@MacBook-Air-Georgij.local>
2023-11-20 14:12:45 +0300

    first

--- src/skills/AerialAce.java
+++ src/skills/AerialAce.java
@@ -1,17 +1,17 @@
 package skills;
 
-import ru.ifmo.se.pokemon.PhysicalMove;
-import ru.ifmo.se.pokemon.Type;
+import ru.ifmo.se.pokemon.*;
 
 public class AerialAce extends PhysicalMove {
     public AerialAce(){
         super(Type.FLYING,60, Double.POSITIVE_INFINITY);
     }
 
+
     @Override
     protected String describe(){
         return "Использована физическая атака AerialAce";
     }
 
-
+    
 }
